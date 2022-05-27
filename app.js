712e677ae8a1f1bbfeb006926ba256b603adb9b4
Iago Vieira <iagovieira@MacBook-Air-de-Iago.local>
2022-05-27 03:28:49 -0300

    connection with database

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app =  express();
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/htmls/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/cadastro", (req, res) =>{
-    res.sendFile(__dirname + "/htmls/Cad.html")
+    res.sendFile(__dirname + "/views/Cad.html")
 });
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/images', express.static(__dirname + 'public/images'));
+
+//conexão com o banco de dados:
+const BD = require('./models/db')
 
 app.listen(8080, () =>{
     console.log("servido iniciado na porta 8080: http://localhost:8080")
---
+++ models/db.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const BD = new Sequelize("clientes","root","Edvania.2020*", {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+    BD.authenticate().then(()=>{
+        console.log(`conexão realizada com sucesso!`);
+    });
+    
+}
+catch(erro){
+    console.erro(`conexão sem sucesso, pois apresenta o erro ${erro}.`);
+}
+    
+
+
+module.exports =  BD;
