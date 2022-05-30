4c11d06eb258dd87aafeab45a5bce9c61eecb3ce
Iago Vieira <iago.unijp@gmail.com>
2022-05-30 12:58:32 -0300

    resolução do cadastramento de dados no DB

--- models/db.js
+++ models/db.js
@@ -8,11 +8,9 @@ const BD = new Sequelize("clientes","root","Edvania.2020*", {
 try {
     BD.authenticate().then(()=>{
         console.log(`conexão realizada com sucesso!`);
-    });
-    
-}
-catch(erro){
+    });   
+}catch(erro){
     console.erro(`conexão sem sucesso, pois apresenta o erro ${erro}.`);
 }
 
-module.exports =  BD;
+module.exports = BD;
